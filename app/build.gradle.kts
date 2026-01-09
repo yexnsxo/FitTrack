e536aa41e28ae1c4a9bc3bbb7f443bf0a0cdb4fb
yexnsxo <yslove829@naver.com>
2026-01-09 10:10:01 +0900

    Feat(#1): 하단 탭바 구현

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,6 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation("androidx.compose.material:material-icons-extended")
 }
